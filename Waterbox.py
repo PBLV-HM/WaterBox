42dbc0c03985861d8a00335005cec2fe60a0e633
FlorianFrank <ffrank@hm.edu>
2017-02-03 13:53:56 +0100

    Waterbox: Reads data from the sensors in an defined intervall and sends it to the server  - get_temperature_humidity fixed

--- Waterbox.py
+++ Waterbox.py
@@ -1,15 +1,23 @@
-import json
 import time
+
+import Adafruit_DHT
 
 import  RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+# GPIO Ports on the PI
 SONIC_TIME_TRIGGER = 18
 SONIC_TIME_ECHO = 24
+TEMPERATURE_SENSOR = 23
 
+# Setup sonic sensor
 GPIO.setup(SONIC_TIME_TRIGGER, GPIO.OUT)
 GPIO.setup(SONIC_TIME_ECHO, GPIO.IN)
+
+# Setup humidity-, temperature-sensor
+humiditytempsensor = Adafruit_DHT.DHT22
+
 
 def get_sonic_time():
     """HCSR04 sends a sonic signal and is notified when the signal returns
@@ -18,39 +26,49 @@ def get_sonic_time():
     :return: time between sending and receiving the sonic signal
     """
 
-    GPIO.output(SONIC_TIME_TRIGGER,True)
+    GPIO.output(SONIC_TIME_TRIGGER, True)
 
     time.sleep(0.00001)
-    GPIO.output(SONIC_TIME_TRIGGER,False)
+    GPIO.output(SONIC_TIME_TRIGGER, False)
 
     starttime = time.time()
     stoptime = time.time()
 
+    counter = 0
+
     while GPIO.input(SONIC_TIME_ECHO) == 0:
         starttime = time.time()
+        counter += 1
+        if counter > 100000:
+            return -1
+
+    counter = 0
 
     while GPIO.input(SONIC_TIME_ECHO) == 1:
         stoptime = time.time()
+        counter += 1
+        if counter > 100000:
+            return -1
 
     return stoptime - starttime
 
-def get_temperature():
+
+def get_temperature_humidity():
     """Gets data from the AM2302 humidity and temperature sensor
        and returns the data in a tupel which contains humidity and temperature
 
     :return: tupel with humidity and temperature
     """
-    sensor = Adafruit_DHT.DHT22
-    gpio = 4
 
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-    returnlist = humidity,temperature
+    humidity, temperature = Adafruit_DHT.read_retry(humiditytempsensor, TEMPERATURE_SENSOR)
+    returnlist = humidity, temperature
     return returnlist
 
 
 def get_air_pressure():
 
     return 0
+
 
 def get_gps_data():
     return 0
@@ -59,10 +77,13 @@ def get_gps_data():
 if __name__ == "__main__":
     while True:
         sonictime = get_sonic_time()
-        print("Distance: ",(sonictime*34300)/2)
-        temperature = get_temperature()
+        if sonictime != -1:
+            print("Distance: {distance} cm\r\n".format(distance=(sonictime*34300)/2))
+        temperature = get_temperature_humidity()
+        print("Temprature {temp} degree\r\n".format(temp =temperature[1]))
+        print ("Humidity {humidity} %\r\n".format(humidity = temperature[0]))
         airpressure = get_air_pressure()
-        gpsdata = get_gps_data()
-        measurementValues = json.dumps(
-            dict(sonic_time=sonictime, temperature=temperature, airpressure=airpressure, gpsdata=gpsdata))
-        time.sleep(1)
+        #gpsdata = get_gps_data()
+        #measurementValues = json.dumps(
+         #   dict(sonic_time=sonictime, temperature=temperature, airpressure=airpressure, gpsdata=gpsdata))
+        time.sleep(2)
